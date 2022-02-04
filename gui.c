fee4bce52e43c1af2514af065d3624486b93ebb4
R4tonBaveur <nebougepasstp@gmail.com>
2022-02-04 18:21:04 +0100

    basic zoom implementation

--- gui.c
+++ gui.c
@@ -7,32 +7,40 @@
 GtkWidget* Window;
 GtkWidget* MenuBar;
 GtkWidget* Open;
+GtkWidget* Selection;
+GtkWidget* Filters;
+GtkWidget* Drawing;
+GtkWidget* Actions;
 GtkWidget* DrawingArea;
-GtkWidget* Layout;
+GtkWidget* Layout; 
 SDL_Surface* Surface;
-
+int Zoom = 50;
 // Callback Functions
 void on_open(){
     printf("Open Clicked\n");
 }
 
 void on_draw(GtkWidget *widget, cairo_t *cr, gpointer data) {
+    double step = (double) 100/Zoom;
+    int fillSize = 1;
+    if(Zoom>100){
+        fillSize = Zoom/100;
+    }
     printf("on draw called\n");
-    guint width, height;
+    int width, height;
     Uint8 r,g,b;
     Uint32 C;
-    width = gtk_widget_get_allocated_width (widget);
-    height = gtk_widget_get_allocated_height (widget);
-    for(int x=0;x<Surface->w && x<width;x++){
-        for(int y=0;y<Surface->h && y<height;y++){
-            C = getPixel(Surface,x,y);
+    width = Surface->w;//gtk_widget_get_allocated_width(widget);
+    height = Surface->h;//gtk_widget_get_allocated_height(widget);
+    for(double x=0;x<width;x+=step){
+        for(double y=0;y<height;y+=step){
+            C = getPixel(Surface, (int) x, (int) y);
             SDL_GetRGB(C, Surface->format,&r, &g, &b);
             cairo_set_source_rgb(cr,
                     (double) r/(double) 255,
                     (double) g/(double) 255,
                     (double) b/(double) 255);
-
-            cairo_rectangle(cr,x,y,1,1);
+            cairo_rectangle(cr, (int) x/step,(int) y/step, fillSize , fillSize);
             cairo_fill(cr);
         }
     }
@@ -66,10 +74,14 @@ int main(int argc, char **argv){
     MenuBar = GTK_WIDGET(gtk_builder_get_object(builder,"MenuBar"));
     Open = GTK_WIDGET(gtk_builder_get_object(builder,"Open"));
     Layout = GTK_WIDGET(gtk_builder_get_object(builder,"BoxLayout"));
-    DrawingArea = GTK_WIDGET(gtk_builder_get_object(builder,"DrawingArea"));
+    Selection = GTK_WIDGET(gtk_builder_get_object(builder,"Selection"));
+    Filters = GTK_WIDGET(gtk_builder_get_object(builder,"Filters"));
+    Drawing = GTK_WIDGET(gtk_builder_get_object(builder,"Drawing"));
+    Actions = GTK_WIDGET(gtk_builder_get_object(builder,"Actions"));
+    DrawingArea = GTK_WIDGET(gtk_builder_get_object(builder,"DrawingArea"));    
     
-
     // Displaying the window
+    gtk_window_set_default_size(GTK_WINDOW(Window),Surface->w+20,Surface->h+20);
     gtk_widget_show_all(Window);
     gtk_widget_show(Window);
 
@@ -78,6 +90,7 @@ int main(int argc, char **argv){
     g_signal_connect(Window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
     g_signal_connect(Open,"activate", G_CALLBACK(on_open), NULL);
     g_signal_connect(DrawingArea,"draw", G_CALLBACK(on_draw), NULL);
+
 
     // Runs the main loop.
     gtk_main();
