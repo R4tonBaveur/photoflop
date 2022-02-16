a95294ff5d158f955f3d86f7e0a0d349d81c8e10
syrinefendi <72197610+syrinefendi@users.noreply.github.com>
2022-02-16 18:36:22 +0100

    Add files via upload

--- draw.c
+++ draw.c
@@ -5,6 +5,20 @@
 #include <stdlib.h>
 #include "random.h"
 
+
+void draw_random_lines(int nr_lines, bool randomizeColor, SDL_Renderer *renderer)
+{
+	SDL_Surface* image = IMG_Load("./images/cat.jpg");
+	for(int i =0; i< nr_lines; i++)
+	{
+		if(randomizeColor)
+		{
+			SDL_SetRenderDrawColor(renderer, randomColor(), randomColor(), randomColor(), 255);
+		}
+		SDL_RenderDrawLine(renderer, randomNumber(0, image->w), randomNumber(0, image->h), randomNumber(0, image->w), randomNumber(0, image->h));
+	}
+}
+
 int main(int argc, char **argv)
 {
 	SDL_Surface* image = IMG_Load("./images/cat.jpg");
@@ -39,8 +53,14 @@ int main(int argc, char **argv)
 		 //Draw points
 		 //SDL_RenderDrawPoint(renderer,randomNumber(0, image->w), randomNumber(0,image->h));
 		 //Draw rectangle 
-		 SDL_Rect rect = { 50, 50, 200, 200};
-		 SDL_RenderFillRect(renderer, &rect);
+		 //SDL_Rect rect = { 50, 50, 200, 200};
+		 //SDL_RenderFillRect(renderer, &rect);
+		 //Draw lines 
+		 //draw_random_lines(5, true, renderer);
+		 //DRaw another rectangle 
+		 //SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); 
+		 //SDL_Rect rect2 = {250, 300, 200, 250};
+		 //SDL_RenderDrawRect(renderer, &rect2);
 
 		 //Show what was drawn 
 		 SDL_RenderPresent(renderer);
