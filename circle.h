1dee8141f719869179dd0baa216fe104984110c5
syrinefendi <72197610+syrinefendi@users.noreply.github.com>
2022-04-04 21:48:01 +0200

    Add files via upload

---
+++ circle.h
@@ -0,0 +1,7 @@
+#ifndef CIRCLE_H
+#define CIRCLE_H
+
+int SDL_RenderDrawcircle(SDL_Renderer* renderer, int x, int y, int radius);
+int SDL_RenderFillCircle(SDL_Renderer* renderer, int x, int y, int radius);
+
+#endif
--- draw.c
+++ draw.c
@@ -3,10 +3,12 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "random.h"
+#include "circle.h"
+#define PI 3.14159265
 
-
-void draw_random_lines(int nr_lines, bool randomizeColor, SDL_Renderer *renderer)
+/*void draw_random_lines(int nr_lines, bool randomizeColor, SDL_Renderer *renderer)
 {
 	SDL_Surface* image = IMG_Load("./images/cat.jpg");
 	for(int i =0; i< nr_lines; i++)
@@ -17,7 +19,21 @@ void draw_random_lines(int nr_lines, bool randomizeColor, SDL_Renderer *renderer
 		}
 		SDL_RenderDrawLine(renderer, randomNumber(0, image->w), randomNumber(0, image->h), randomNumber(0, image->w), randomNumber(0, image->h));
 	}
-}
+}*/
+/*
+//set window color 
+int setWindowColor(SDL_Renderer *renderer, SDL_Color color)
+{
+	if ( SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a) < 0)
+	{
+		return -1;
+	}
+	if ( SDL_RenderClear(renderer) < 0 )
+	{
+		return -1
+	}
+	return 0;
+}*/
 
 int main(int argc, char **argv)
 {
@@ -30,7 +46,7 @@ int main(int argc, char **argv)
         SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, image->w, image->h, 0);
 	// create a renderer 
 	 SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-
+	 SDL_Point point[5];
 	 bool running = true;
 	 SDL_Event event; 
 	 while(running)
@@ -49,18 +65,23 @@ int main(int argc, char **argv)
 		 SDL_RenderClear(renderer);
 
 		 //Draw
-		 SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); //Yellow
+		 SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); //Yellow
 		 //Draw points
-		 //SDL_RenderDrawPoint(renderer,randomNumber(0, image->w), randomNumber(0,image->h));
+		 //SDL_RenderDrawPoints(renderer,point,640);
+		 //DRaw lines
+		 //SDL_RenderDrawLine(renderer, 0, 0, image->w, image->h);
+		 //SDL_RenderPresent(renderer);
 		 //Draw rectangle 
 		 //SDL_Rect rect = { 50, 50, 200, 200};
 		 //SDL_RenderFillRect(renderer, &rect);
-		 //Draw lines 
-		 //draw_random_lines(5, true, renderer);
 		 //DRaw another rectangle 
 		 //SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); 
-		 //SDL_Rect rect2 = {250, 300, 200, 250};
-		 //SDL_RenderDrawRect(renderer, &rect2);
+		 SDL_Rect rect2 = {100, 100, 100, 100};
+		 SDL_RenderDrawRect(renderer, &rect2);
+		// Draw a circle 
+		/*SDL_RenderDrawcircle(renderer, 200, 200, PI);
+		SDL_RenderFillCircle(renderer, 200, 200, PI);*/
+
 
 		 //Show what was drawn 
 		 SDL_RenderPresent(renderer);
@@ -70,7 +91,5 @@ int main(int argc, char **argv)
 	 SDL_DestroyRenderer(renderer);
 	 SDL_DestroyWindow(window);
 	 SDL_Quit();
-
 	 return 0;
-
 }
