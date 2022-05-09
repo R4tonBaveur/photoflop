9351fff9dd1744fa8d9d233efc41d86843afcf81
R4tonBaveur <nebougepasstp@gmail.com>
2022-05-09 12:48:20 +0200

    many stuffs

--- gui.c
+++ gui.c
@@ -5,7 +5,10 @@
 #include "C-Utils/stack/stack.h"
 #include <stdlib.h>
 #include <math.h>
-
+struct SStack{
+    struct SStack* old;
+    SDL_Surface* surface;
+};
 // GTK items
 GtkWidget* Window;
 GtkWidget* MenuBar;
@@ -21,10 +24,49 @@ GtkWidget* DarknessButton;
 GtkWidget* BrightnessButton;
 GtkWidget* BlurButton;
 GtkWidget* GoBackButton;
-GtkWidget* OpenButton;
+GtkWidget* SaveButton;
+GtkWidget* RectangleButton;
+struct SStack* s;
 SDL_Surface* Surface;
 SDL_Surface* SelectionZone;
-int SurfaceVersion;
+char* filename;
+
+void get_coord(SDL_Surface* image, size_t *x, size_t *y)
+{
+    static int counter = 0;
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window * window = SDL_CreateWindow("Photoflop",
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, image->w, image->h, 0);
+    SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, 0);
+
+    SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer,
+        image);
+
+    int quit = 0;
+    SDL_Event event;
+    while (!quit)
+    {
+        SDL_WaitEvent(&event);
+
+        switch (event.type)
+        {
+        case SDL_QUIT:
+            quit = 1;
+            break;
+        case SDL_MOUSEBUTTONDOWN:
+            *x = event.motion.x;
+            *y = event.motion.y;
+            quit = 1;
+            break;
+        }
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+}
 
 // Resize the Surface to fit the app
 SDL_Surface* resize(SDL_Surface* image){
@@ -48,18 +90,10 @@ SDL_Surface* resize(SDL_Surface* image){
 }
 
 // Callback Functions
-void on_open(){
-    printf("Open Clicked\n");
-}
 
 void on_draw(GtkWidget *widget, cairo_t *cr, gpointer data){
-    printf("on draw called\n");
-    if(Surface)
-        printf("Surface initialized\n");
-    else{
-        printf("No Surface");
+    if(!Surface)
         return;
-    }
     int width, height;
     Uint8 r,g,b;
     Uint32 C;
@@ -79,18 +113,23 @@ void on_draw(GtkWidget *widget, cairo_t *cr, gpointer data){
     }
     cairo_stroke(cr);
 }
+void update(){
+    struct SStack* new = malloc(sizeof(struct SStack));
+    SDL_Surface* newSurface = SDL_CreateRGBSurface(0,Surface->w,Surface->h,32,0,0,0,0);
+    for(int x=0;x<Surface->w;x++){
+        for(int y=0;y<Surface->h;y++){
+            setPixel(newSurface,x,y,getPixel(Surface,x,y));
+        }
+    }
+    new->old = s;
+    new->surface = newSurface;
+    s = new;
+}
 
 void on_GoBack(){
-    printf("go back\n");
-    char path[64];
-    SurfaceVersion--;
-    printf("1\n");
-    sprintf(path,"%d.jpg\0",SurfaceVersion);
-    printf("%s\n",path);
-    Surface = IMG_Load(path);
-    printf("3%d\n",Surface->w);
+    s = s->old;
+    Surface = s->surface;
     gtk_widget_queue_draw(DrawingArea);
-    printf("4\n");
 }
 
 void on_GrayScale(){
@@ -105,10 +144,12 @@ void on_GrayScale(){
             SDL_GetRGB(pixel, Surface->format, &r, &g, &b);
             Uint8 avg = 0.3*r+0.59*g+0.11*b;
             pixel = SDL_MapRGB(Surface->format, avg, avg, avg);
+            if(getPixel(SelectionZone,x,y))
                 setPixel(Surface, x, y, pixel);
         }
     }
     gtk_widget_queue_draw(DrawingArea);
+    update();
 }
 
 void on_Sepia(){
@@ -123,6 +164,7 @@ void on_Sepia(){
             setPixel(Surface, x, y, pixel);
         }
     }
+    update();
     gtk_widget_queue_draw(DrawingArea);
 
 }
@@ -140,6 +182,7 @@ void on_Darkness(){
             setPixel(Surface, x, y, pixel);
         }
     }
+    update();
     gtk_widget_queue_draw(DrawingArea);
 }
 
@@ -156,6 +199,7 @@ void on_Brightness(){
             setPixel(Surface, x, y, pixel);
         }
     }
+    update();
     gtk_widget_queue_draw(DrawingArea);
 }
 
@@ -188,16 +232,49 @@ void on_Blur(){
             setPixel(image,x,y,matrix[x][y]);
         }
     }
+    update();
     gtk_widget_queue_draw(DrawingArea);
 }
 
+void on_save(){
+    char path[64];
+    sprintf(path,"exports/%s",filename);
+    saveImage(Surface,path);
+}
 
-void print_int(void* val){
-    int * p = val;
-    printf("%d",*p);
+void on_Rectangle(){
+    size_t x1,y1,x2,y2;
+    get_coord(Surface,&x1,&y1);
+    get_coord(Surface,&x2,&y2);
+    printf("%zu\n",x1);
+    printf("%zu\n",y1);
+    printf("%zu\n",x2);
+    printf("%zu\n",y2);
+    if(x2<x1){
+        size_t mem = x1;
+        x1 = x2;
+        x2 = mem;
+    }
+    if(y2<y1){
+        size_t mem = y1;
+        y1 = y2;
+        y2 = mem;
+    }
+    for(int x=0;x<SelectionZone->w;x++){
+        for(int y=0;y<SelectionZone->h;y++){
+            if(x>=x1 && x<=x2 && y>=y1 && y<=y2){
+                setPixel(SelectionZone,x,y,RGBToUint32(SelectionZone,255,255,255));
+            } else {
+                setPixel(SelectionZone,x,y,0);
+            }
+        }
+    }
 }
 // Main function
-int main(/*int argc, char **argv*/){
+int main(int argc, char **argv){
+    if(argc<2){
+        errx(1,"Usage: ./gui <filename>");
+    }
     // Initializes GTK
     gtk_init(NULL, NULL);
 
@@ -212,12 +289,12 @@ int main(/*int argc, char **argv*/){
     }
 
     // The image
-    Surface = resize(loadImage("images/cat.jpg"));
+    filename = argv[1];
+    Surface = resize(loadImage(filename));
     if(Surface==NULL){
         printf("Error while loading surface\n");
         return 1;
     }
-    SurfaceVersion = 0;
     // The zone image
     SelectionZone = SDL_CreateRGBSurface(0,Surface->w,Surface->h,32,0,0,0,0);
     printf("selection zone allocated\n");
@@ -227,10 +304,16 @@ int main(/*int argc, char **argv*/){
         }
     }
     
+    // Surface stack
+    s = malloc(sizeof(struct SStack));
+    s->old = NULL;
+    s->surface = Surface;
+    update();
+
     // Getting the different components from the builder
     Window = GTK_WINDOW(gtk_builder_get_object(builder, "Window"));
     MenuBar = GTK_WIDGET(gtk_builder_get_object(builder,"MenuBar"));
-    OpenButton = GTK_WIDGET(gtk_builder_get_object(builder,"OpenButton"));
+    SaveButton = GTK_WIDGET(gtk_builder_get_object(builder,"SaveButton"));
     Layout = GTK_WIDGET(gtk_builder_get_object(builder,"BoxLayout"));
     Selection = GTK_WIDGET(gtk_builder_get_object(builder,"Selection"));
     Filters = GTK_WIDGET(gtk_builder_get_object(builder,"Filters"));
@@ -243,7 +326,7 @@ int main(/*int argc, char **argv*/){
     BrightnessButton = GTK_WIDGET(gtk_builder_get_object(builder,"BrightnessButton"));
     GoBackButton = GTK_WIDGET(gtk_builder_get_object(builder,"GoBackButton"));
     BlurButton = GTK_WIDGET(gtk_builder_get_object(builder,"BlurButton"));
-
+    RectangleButton = GTK_WIDGET(gtk_builder_get_object(builder,"RectangleButton"));
     // Displaying the window
     gtk_window_set_default_size(GTK_WINDOW(Window),500,500);
     gtk_window_set_resizable(Window, FALSE);    
@@ -253,7 +336,7 @@ int main(/*int argc, char **argv*/){
     
     // Connecting the signals
     g_signal_connect(Window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-    g_signal_connect(OpenButton,"activate", G_CALLBACK(on_open), NULL);
+    g_signal_connect(SaveButton,"activate", G_CALLBACK(on_save), NULL);
     g_signal_connect(DrawingArea,"draw", G_CALLBACK(on_draw), NULL);
     g_signal_connect(GoBackButton,"activate",G_CALLBACK(on_GoBack), NULL);
     g_signal_connect(GrayScaleButton,"activate",G_CALLBACK(on_GrayScale), NULL);
@@ -261,11 +344,11 @@ int main(/*int argc, char **argv*/){
     g_signal_connect(DarknessButton,"activate",G_CALLBACK(on_Darkness), NULL);
     g_signal_connect(BrightnessButton,"activate",G_CALLBACK(on_Brightness), NULL);
     g_signal_connect(BlurButton,"activate",G_CALLBACK(on_Blur), NULL);
+    g_signal_connect(RectangleButton,"activate",G_CALLBACK(on_Rectangle), NULL);
 
     // Runs the main loop.
     gtk_main();
     gtk_widget_destroy(Window);
-    gtk_widget_destroy(OpenButton);
     gtk_widget_destroy(MenuBar);
     // Exits.
     return 0;
